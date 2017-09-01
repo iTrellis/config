28025ee48242ab09054bc45362b6f32a1905b3fc
Henry Huang <hhh@rutcode.com>
2017-09-01 11:48:01 +0800

    lost file : options.go

---
+++ options.go
@@ -0,0 +1,32 @@
+// GNU GPL v3 License
+
+// Copyright (c) 2017 github.com:go-trellis
+
+package config
+
+// Options initial params
+type Options map[string]interface{}
+
+// String return string(value) with key in options
+func (p Options) String(key string) (string, error) {
+	if 0 == len(p) {
+		return "", nil
+	}
+	return defaultGetter.GetMapKeyValueString(p, key)
+}
+
+// Int return int(value) with key in options
+func (p Options) Int(key string) (int, error) {
+	if 0 == len(p) {
+		return 0, nil
+	}
+	return defaultGetter.GetMapKeyValueInt(p, key)
+}
+
+// Int64 return int64(value) with key in options
+func (p Options) Int64(key string) (int64, error) {
+	if 0 == len(p) {
+		return 0, nil
+	}
+	return defaultGetter.GetMapKeyValueInt64(p, key)
+}
